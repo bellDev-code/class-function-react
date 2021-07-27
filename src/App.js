0fbf0a64b00b16633fdf2b37678d58db76b377b6
bellDev-code <sumaoo20@naver.com>
2021-07-27 18:35:05 +0900

    class component state setting, initialization, change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>hello world!</h1>
+      {/* initNumber을 컴포넌트에 넣어줌 */}
+      <FuncComp initNumber={2}></FuncComp>
+      <ClassComp initNumber={2}></ClassComp>
     </div>
   );
+}
+
+// 자기 자신이 render 된다.
+function FuncComp(num) {
+  return (
+    <div className="container">
+      <h2>function style component</h2>
+      <p>Number : {num.initNumber}</p>
+    </div>
+  );
+}
+
+// render method를 정의해서 리턴 값이 UI가 된다.
+// function component에서 state 초기화, 사용, 변경이 안됐다.
+// 그러나, hook을 통해 function component에서도 쓸 수 있다.
+class ClassComp extends React.Component {
+  // state 초기화
+  state = {
+    number: this.props.initNumber,
+  };
+  render() {
+    return (
+      <div className="container">
+        <h2>class style component</h2>
+        {/* state 사용 */}
+        <p>Number : {this.state.number}</p>
+        <input
+          type="button"
+          value="random"
+          onClick={function () {
+            // state의 변경
+            this.setState({ number: Math.random() });
+          }.bind(this)}
+        ></input>
+      </div>
+    );
+  }
 }
 
 export default App;
