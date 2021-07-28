f70ad1add8e890eec0b76a93643f8260755990b5
bellDev-code <sumaoo20@naver.com>
2021-07-28 14:16:30 +0900

    reat hooks practice

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
@@ -13,11 +13,41 @@ function App() {
 }
 
 // 자기 자신이 render 된다.
-function FuncComp(num) {
+function FuncComp(props) {
+  // useState를 쓰면 2개의 값을 가진 배열이 선언된다. 인자로 초기값
+  // const numberState = useState(props.initNumber);
+  // const number = numberState[0];
+  // const setNumber = numberState[1];
+
+  // 위의 코드의 축약형이다.
+  const [number, setNumber] = useState(props.initNumber);
+
+  // const dateState = useState(new Date().toString());
+  // const date = dateState[0];
+  // const setDate = dateState[1];
+
+  // 위의 코드의 축약형이다.
+  const [date, setDate] = useState(new Date().toString());
+
   return (
     <div className="container">
       <h2>function style component</h2>
-      <p>Number : {num.initNumber}</p>
+      <p>Number : {number}</p>
+      <p>Date : {date}</p>
+      <input
+        type="button"
+        value="random"
+        onClick={function () {
+          setNumber(Math.random());
+        }}
+      ></input>
+      <input
+        type="button"
+        value="date"
+        onClick={function () {
+          setDate(new Date().toString());
+        }}
+      ></input>
     </div>
   );
 }
@@ -29,6 +59,7 @@ class ClassComp extends React.Component {
   // state 초기화
   state = {
     number: this.props.initNumber,
+    date: new Date().toString(),
   };
   render() {
     return (
@@ -36,12 +67,20 @@ class ClassComp extends React.Component {
         <h2>class style component</h2>
         {/* state 사용 */}
         <p>Number : {this.state.number}</p>
+        <p>Date: {this.state.date}</p>
         <input
           type="button"
           value="random"
           onClick={function () {
             // state의 변경
             this.setState({ number: Math.random() });
+          }.bind(this)}
+        ></input>
+        <input
+          type="button"
+          value="date"
+          onClick={function () {
+            this.setState({ date: new Date().toString() });
           }.bind(this)}
         ></input>
       </div>
